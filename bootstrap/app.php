fb8cd07b0cc141dbabd26078570330e7fa769d8a
dils <dilsdev55@gmail.com>
2026-08-13 11:51:30 +0700

    fix(auth): register Spatie middleware aliases in bootstrap/app.php to fix Issue #14

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \App\Http\Middleware\ForceJsonResponse::class,
         ]);
+
+        // Register Spatie Permission Middleware aliases
+        $middleware->alias([
+            'role'               => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission'         => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Force JSON response for API exceptions
